f2481fb95f983a294be08897d3075a17bf1bd3c4
ryandougc <ryan.carr@emailkpu.ca>
2020-05-05 23:56:24 -0700

    Responsive CSS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express       = require('express')
 const app           = express()
-server              = require('http').Server(app)
+const server        = require('http').Server(app)
+const port          = process.env.PORT || 5000
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
@@ -15,4 +16,4 @@ app.get('*', (req, res) => {
     res.redirect('/')
 })
 
-server.listen(process.env.PORT || 8080, () => console.log('Running'))
+server.listen(port, () => console.log(`App is running on port ${port}...`))
